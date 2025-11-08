dcef3e700f72c153c191760fc60b62e850697894
Lerunya11 <kachanovavaleriia@gmail.com>
2025-11-08 01:09:24 +0100

    Remove value prop and use defaultValue in SearchBox

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,21 +1,22 @@
+// src/components/SearchBox/SearchBox.tsx
 import { useId } from 'react';
 import css from './SearchBox.module.css';
 
 export interface SearchBoxProps {
-  value: string;
   onChange: (value: string) => void;
   placeholder?: string;
 }
 
-export default function SearchBox({ value, onChange, placeholder }: SearchBoxProps) {
+export default function SearchBox({ onChange, placeholder }: SearchBoxProps) {
   const id = useId();
+
   return (
     <input
       id={id}
       name="search"
       className={css.input}
       type="text"
-      value={value}
+      defaultValue=""
       onChange={(e) => onChange(e.target.value.trimStart())}
       placeholder={placeholder ?? 'Search notes'}
       autoComplete="off"
