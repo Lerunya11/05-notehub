9e3539054915169ca3755ccac6f27b74fb9f93e6
Lerunya11 <kachanovavaleriia@gmail.com>
2025-11-08 01:48:14 +0100

    Update SearchBox.tsx

--- src/components/SearchBox/SearchBox.tsx
+++ src/components/SearchBox/SearchBox.tsx
@@ -1,22 +1,25 @@
-// src/components/SearchBox/SearchBox.tsx
 import { useId } from 'react';
 import css from './SearchBox.module.css';
 
 export interface SearchBoxProps {
+  value: string;
   onChange: (value: string) => void;
   placeholder?: string;
 }
 
-export default function SearchBox({ onChange, placeholder }: SearchBoxProps) {
+export default function SearchBox({
+  value,
+  onChange,
+  placeholder,
+}: SearchBoxProps) {
   const id = useId();
-
   return (
     <input
       id={id}
       name="search"
       className={css.input}
       type="text"
-      defaultValue=""
+      value={value}
       onChange={(e) => onChange(e.target.value.trimStart())}
       placeholder={placeholder ?? 'Search notes'}
       autoComplete="off"
